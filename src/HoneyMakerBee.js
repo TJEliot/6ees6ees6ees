7ffbdda075d735ee1e5e5a538dd50958312dde80
bgaesop <bgaesop@gmail.com>
2017-12-13 19:41:16 -0800

    done

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor() {
+    super();
+    this.age = this.age * 2;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+  
+  makeHoney() {
+    this.honeyPot++;
+  }
+  
+  giveHoney() {
+    this.honeyPot--;
+  }
 };
